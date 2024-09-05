7045e26bd3e275933c545e51d40b39cb4f603f5f
KiryushE <kirillzagorodnij419@gmail.com>
2024-09-06 02:14:56 +0300

    Переробка дизайну

--- app/routes.py
+++ app/routes.py
@@ -71,6 +71,8 @@ def delete_pet(pet_id):
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
